0a44f947981ed7c64f8812448b81507a4e160103
Mohammed <mohammedemad557@yahoo.com>
2026-04-25 11:02:34 +0300

    validations

--- lib/screens/info_screen.dart
+++ lib/screens/info_screen.dart
@@ -18,7 +18,7 @@ class _InfoScreenState extends State<InfoScreen> {
   final TextEditingController dateController = TextEditingController();
   final TextEditingController heightController = TextEditingController();
   final TextEditingController weightController = TextEditingController();
-
+  GlobalKey<FormState> key = GlobalKey<FormState>();
   int? gender;
 
   @override
@@ -29,111 +29,138 @@ class _InfoScreenState extends State<InfoScreen> {
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 16.0),
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Text(
-                  "BMI",
-                  style: TextStyle(
-                    letterSpacing: 10,
-                    fontWeight: FontWeight.w900,
-                    fontSize: 36,
-                    color: Color(0xff01502E),
+            child: Form(
+              key: key,
+              child: Column(
+                children: [
+                  Text(
+                    "BMI",
+                    style: TextStyle(
+                      letterSpacing: 10,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 36,
+                      color: Color(0xff01502E),
+                    ),
                   ),
-                ),
-                SizedBox(height: 45),
+                  SizedBox(height: 45),
 
-                CustomTextFormField(
-                  nameController: nameController,
-                  title: "Name",
-                  hint: "Name",
-                  onChanged: (value) {
-                    print(value);
-                  },
-                ),
-                SizedBox(height: 20),
-
-                CustomTextFormField(
-                  nameController: dateController,
-                  hint: "Birthdate",
-                  title: "Birthdate",
-                  readOnly: true,
-                  onTap: () async {
-                    print("Birthdate field taped");
-                    var res = await showDatePicker(
-                      context: context,
-                      firstDate: DateTime(2010),
-                      lastDate: DateTime(2030),
-                    );
-                    print(res);
-                    if (res != null) {
-                      String date = res.toIso8601String();
-
-                      dateController.text = date.substring(0, 10);
-                    }
-                  },
-                ),
-                SizedBox(height: 36),
-
-                GenderSelection(
-                  selectedGender: (value) {
-                    print("the gender is:$value");
-                    gender = value;
-                  },
-                ),
-
-                SizedBox(height: 40),
-                CalculatedTextFormField(
-                  title: "Your Height(cm)",
-                  hint: "Your Height",
-                  controller: heightController,
-                ),
-                SizedBox(height: 14),
-                CalculatedTextFormField(
-                  title: "Your Weight(cm)",
-                  hint: "Your Weight",
-                  controller: weightController,
-                ),
-                SizedBox(height: 40),
-                SizedBox(
-                  width: 400,
-                  child: ElevatedButton(
-                    onPressed: () async {
-
-                      var res = await fetchBmi(
-                        height:heightController.text,
-                        weight:weightController.text,
-                      );
-                      var bmi = BmiResponse.fromJson(res.data);
-
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ResScreen(
-                            bmi: bmi,
-                            name: nameController.text,
-                            birthdate: dateController.text,
-                          ),
-                        ),
-                      );
+                  CustomTextFormField(
+                    nameController: nameController,
+                    title: "Name",
+                    hint: "Name",
+                    onChanged: (value) {
+                      print(value);
                     },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      backgroundColor: Color(0xff484783),
-                    ),
+                  ),
+                  SizedBox(height: 20),
 
-                    child: const Text(
-                      "Calculate BMI",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
+                  CustomTextFormField(
+                    nameController: dateController,
+                    hint: "Birthdate",
+                    title: "Birthdate",
+                    readOnly: true,
+                    onTap: () async {
+                      print("Birthdate field taped");
+                      var res = await showDatePicker(
+                        context: context,
+                        firstDate: DateTime(2010),
+                        lastDate: DateTime(2030),
+                      );
+                      print(res);
+                      if (res != null) {
+                        String date = res.toIso8601String();
+
+                        dateController.text = date.substring(0, 10);
+                      }
+                    },
+                  ),
+                  SizedBox(height: 36),
+
+                  GenderSelection(
+                    selectedGender: (value) {
+                      print("the gender is:$value");
+                      gender = value;
+                    },
+                  ),
+
+                  SizedBox(height: 40),
+                  CalculatedTextFormField(
+                    title: "Your Height(cm)",
+                    hint: "Your Height",
+                    controller: heightController,
+                    validator: (value){
+                      // final emailRegex = RegExp(
+                      //   r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$',
+                      // );
+                      if(value == null||value.isEmpty){
+                        return "Height field can't be empty";
+
+                      }
+                      return null;
+
+                      // if (!emailRegex.hasMatch(value.trim())) {
+                      //   return 'Enter a valid email address';
+                      // }
+
+                    },
+                  ),
+                  SizedBox(height: 14),
+                  CalculatedTextFormField(
+                    title: "Your Weight(cm)",
+                    hint: "Your Weight",
+                    controller: weightController,
+                    validator: (value){
+                    if(value == null||value.isEmpty){
+                      return "Weight field can't be empty";
+                    }
+                    return null;
+
+                    },
+                  ),
+                  SizedBox(height: 40),
+                  SizedBox(
+                    width: 400,
+                    child: ElevatedButton(
+                      onPressed: () async {
+
+                        if(key.currentState!.validate()){
+                          var res = await fetchBmi(
+                            height:heightController.text,
+                            weight:weightController.text,
+                          );
+                          var bmi = BmiResponse.fromJson(res.data);
+
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ResScreen(
+                                bmi: bmi,
+                                name: nameController.text,
+                                birthdate: dateController.text,
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        backgroundColor: Color(0xff484783),
+                      ),
+
+                      child: const Text(
+                        "Calculate BMI",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
@@ -153,11 +180,11 @@ class _InfoScreenState extends State<InfoScreen> {
       return res;
     } on DioException catch (ec) {
       throw ec;
-      print(ec.response);
+
     } catch (e) {
       throw e;
 
-      print("========>${e}");
+
     }
   }
 }
